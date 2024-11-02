1309e7dc4d80c6c93eda46aebda0eda92325d43d
td7legend <td7legend@gmail.com>
2024-11-02 10:09:03 +0700

    Update Staff

--- backend/koi-farm-demo/koi-farm-demo/Controllers/StaffController.cs
+++ backend/koi-farm-demo/koi-farm-demo/Controllers/StaffController.cs
@@ -1,75 +1,64 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
+﻿using koi_farm_demo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace koi_farm_demo.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class StaffController : ControllerBase
-//    {
-//        private readonly IStaffService _staffService;
+namespace koi_farm_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
 
-//        public StaffController(IStaffService staffService)
-//        {
-//            _staffService = staffService;
-//        }
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
 
-//        [HttpGet("all")]
-//        public async Task<IActionResult> GetAllStaff()
-//        {
-//            try
-//            {
-//                var staffs = await _staffService.GetAllStaffAsync();
-//                return Ok(staffs);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllStaff()
+        {
+            try
+            {
+                var staffs = await _staffService.GetAllStaffAsync();
+                return Ok(staffs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-//        [HttpGet("{id}")]
-//        public async Task<IActionResult> GetStaffById(int id)
-//        {
-//            try
-//            {
-//                var staff = await _staffService.GetStaffByIdAsync(id);
-//                return Ok(staff);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetStaffById(int id)
+        //{
+        //    try
+        //    {
+        //        var staff = await _staffService.GetStaffByIdAsync(id);
+        //        return Ok(staff);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-       
 
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> UpdateStaff(int id, [FromBody] UpdateStaffModel model)
-//        {
-//            try
-//            {
-//                await _staffService.UpdateStaffAsync(id, model);
-//                return Ok();
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
 
-//        [HttpDelete("{id}")]
-//        public async Task<IActionResult> DeleteStaff(int id)
-//        {
-//            try
-//            {
-//                await _staffService.DeleteStaffAsync(id);
-//                return Ok();
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message);
-//            }
-//        }
-//    }
-//}
+        
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteStaff(int id)
+        //{
+        //    try
+        //    {
+        //        await _staffService.DeleteStaffAsync(id);
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+    }
+}
